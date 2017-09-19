81804a27474faf2624190a8a50a71c9b09bd188c
bulletproofua <yuriy.haydeychuk@gmail.com>
2017-09-20 01:04:55 +0300

    apriori2

--- common/models/apriori-algorithm.js
+++ common/models/apriori-algorithm.js
@@ -6,36 +6,6 @@ var Backbone = require('backbone');
 
 module.exports = function( Apriorialgorithm ) {
 
-// function MingoAggreg( dataForAggr, minRating ) {
-//   var aggr = [
-//     {
-//       $match: {
-//        rating: {
-//          $gt: minRating
-//        }
-//      }
-//    },
-//   //  {
-//   //      $group: {
-//   //        _id: "$id",
-//   //        count: { $sum: 1 }
-//   //      }
-//   //   }
-//   ];
-//
-//   // if (!dataForAggr || !dataForAggr.toJSON) return [];
-//   var data = dataForAggr.toJSON();
-//   // console.log("in method", data.length);
-//   try {
-//     var MingoResult = Mingo.aggregate( data , aggr);
-//   } catch (e) {
-//     return e ;//cb(e);
-//   }
-//   console.log("================= MINGO =============");
-//   return MingoResult;
-// }
-
-
 function getOnlyIds(data){
     var result = [];
 
@@ -50,10 +20,6 @@ function getOnlyIds(data){
     }
     return result;
 };
-
-
-var arrOfItems = [];
-var arrOfCountItems = [];
 
 // var filmId = [146, 149, 151, 163, 164, 168, 146, 164,999];  // movies of all users
 //
@@ -72,7 +38,6 @@ function countSupport(data, itemSets){
     for (var j = 0; j < itemSets.length; j++) {
         itemSetsLocal.push( { id: itemSets[j] , support: 0});
         for (var i = 0; i < data.length; i++) {
-
             if (includes(data[i], itemSetsLocal[j]['id'])){
                 itemSetsLocal[j]['support']++;
             }
@@ -87,15 +52,12 @@ function check(arr){
   for (var i = 0; i < arr.length; i++) {
     for (var j=i+1 ; j < arr.length; j++) {
         if(arr[i].id == arr[j].id){
-          console.log("==========================================")
           res = true;
         }
     }
   }
   return res;
 }
-
-
 
 function contains(array, element){
   for (var i = 0; i < array.length; i++) {
@@ -110,6 +72,7 @@ function createPairs(itemSets){
   for (var i = 0; i < itemSets.length; i++) {
     for (var j = i+1; j < itemSets.length; j++) {
       var itemElement = createItemSet(itemSets[i].id, itemSets[j].id);
+      // console.log('ITEMELEMENT', itemElement)
       if( itemElement != null){
         if( !contains(arrOfCouple, itemElement)){ // перевірити
           arrOfCouple.push( itemElement );
@@ -122,12 +85,15 @@ function createPairs(itemSets){
 
 function createItemSet(itemSet1, itemSet2){
   var uniqueElements = [];
+  // console.log("============== \n")
   if(itemSet1.length != itemSet2.length ) return null;
+
     itemSet1.forEach(function(item){
       if(uniqueElements.indexOf(item) == -1){
         uniqueElements.push(item);
       }
     })
+    // console.log("1 ", itemSet1);
 
 
     itemSet2.forEach(function(item){
@@ -135,42 +101,25 @@ function createItemSet(itemSet1, itemSet2){
         uniqueElements.push(item);
       }
     })
+    // console.log("2 ", itemSet2);
 
+    // console.log('UNIQUEELEMENTS', uniqueElements)
     if( uniqueElements.length == itemSet1.length+1){
-      return uniqueElements.sort(function(a, b) {
-        return a - b;
-      });
+      // console.log('CONDITION PASSED ======>')
+      uniqueElements.sort(
+      //   function(a, b) {
+      //   return a - b;
+      // }
+    );
+
+      return uniqueElements
+      // .sort(function(a, b) {
+      //   return a - b;
+      // });
     } else {
       return null;
     }
 };
-
-
-  var myData = data;
-  var users = _.keys(myData);
-
-
-// набір усі ід фільмів найбільш схожих користувачів
-  var firstItemSetData = [];
-
-    var firstData =[];
-    for(var key in myData){
-        for (var i = 0 ; i < myData[key].length; i++) {
-          var elementOfMyData = [ Number(myData[key][i].id )];
-          if(!contains(firstData , elementOfMyData )){
-            firstData.push( elementOfMyData );
-          }
-        }
-    }
-
-
-
-var itemSet = firstData;
-
-itemSet.sort(function(a,b){
-  return a -b ;
-});
-
 
 function supCountFilter(arr, minCount){
   var dataArr = [];
@@ -183,15 +132,104 @@ function supCountFilter(arr, minCount){
   return dataArr;
 }
 
+function minSupportArr (arr, minCount){
+  var dataArr = [];
+  var maxLength = arr.length;
+  for (var i = 0; i < maxLength; i++) {
+    if(arr[i].support < minCount) {
+      dataArr.push(arr[i].id);
+    }
+  }
+  return dataArr;
+}
 
+function deleteMinSupElements (arrWithPairs, arrWithMinSup) {
+    var itemSet = [];
+    for (var i = 0; i < arrWithPairs.length; i++) {
+      var minScore = 9999999;
+      arrWithPairs[i].length
+      for (var j = 0; j < arrWithMinSup.length; j++) {
+        var Arr = _.uniq(arrWithPairs[i].concat(arrWithMinSup[j]));
+        var minScore = min( minScore, Arr.length );
+        if( minScore != arrWithPairs[i].length && j == arrWithMinSup.length-1){
+          itemSet.push(arrWithPairs[i]);
+        }
+      }
+    }
+    return itemSet;
+};
+
+function min(a, b) {
+  return a < b ? a : b;
+}
+//===================================================================================
+  // початковий набір даних ( з бд )
+  var myData = data;
+  // console.log('MYDATA', myData)
+  var users = _.keys(myData);
+
+
+// набір усі ід фільмів найбільш схожих користувачів
+
+  var itemSet =[];
+  for(var key in myData){
+      for (var i = 0 ; i < myData[key].length; i++) {
+        var elementOfMyData = [ Number(myData[key][i].id )];
+        if(!contains(itemSet , elementOfMyData )){
+          itemSet.push( elementOfMyData );
+        }
+      }
+  }
+
+  itemSet.sort(function(a,b){
+    return a - b ;
+  });
+
+// console.log('ITEMSET', itemSet)
+
+
+
+// ід фільмів по клжному з користувачів [[],[],[],[],[]]
 var finalData = getOnlyIds(myData);
 // console.log('FINALDATA', finalData)
 
-countSupport(finalData, itemSet)
-var supData = countSupport(finalData, itemSet)
-var filteredData = supCountFilter(supData, 2)
-var dataWhithPairs = createPairs(filteredData);
-console.log('AFTERPAIRS', dataWhithPairs)
+// var itemSet = [ ['A'], ['B'], ['C'], ['D'], ['E'] ];
+// var finalData = [
+//   ['A','C','D'],
+//   ['B', 'C', 'E'],
+//   ['A', 'B', 'C', 'E'],
+//   ['B', 'E']
+// ];
+
+var supData, filteredData, minSupArr, dataWhithPairs;
+// function Apriori(dataBaseTDB, itemSet){
+var lastResult = [];
+var support = 3;
+  while (itemSet.length != 1) {
+    supData = countSupport(finalData, itemSet)
+    // console.log('SUPDATA2 \n', supData)
+    // console.log('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n')
+    filteredData = supCountFilter(supData, support)
+    // console.log('FILTEREDDATA \n', filteredData2)
+    minSupArr = minSupportArr(supData, support)
+    // console.log('MINSUPARR', minSupArr)
+     dataWhithPairs = createPairs(filteredData);
+    // console.log('DATAWHITHPAIRS \n', dataWhithPairs2)
+    lastResult = itemSet;
+
+    itemSet = deleteMinSupElements(dataWhithPairs , minSupArr);
+
+    console.log('itemSet', itemSet)
+    console.log('\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n')
+    if(itemSet.length == 0){
+      console.log(_.union(_.flatten(lastResult)));
+      break;
+    }
+  }
+    // console.log('LASTRESULT', lastResult)
+
+// }
+
 
   Apriorialgorithm.remoteMethod( 'Apriorialgorithm', {
     description: "Return data",
